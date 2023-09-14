d4f7bc1dfcc75703846e4e20878421a5633e24b7
borkarSahil <officialsahilborkar@gmail.com>
2023-09-14 19:19:12 +0530

    square

--- Turtle/main.py
+++ Turtle/main.py
@@ -1,9 +1,17 @@
-from turtle import  Turtle, Screen
+from turtle import Turtle, Screen
 
 the_turtle = Turtle()
 
+#  SQUARE  START
 the_turtle.shape("turtle")
 the_turtle.color("red")
+
+for _ in range(4):
+    the_turtle.forward(100)
+    the_turtle.right(90)
+
+#  END
+
 
 screen = Screen()
 screen.exitonclick()
