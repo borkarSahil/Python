3284610e2d85cffcfeeeb7f862344337c49b800b
borkarSahil <officialsahilborkar@gmail.com>
2023-09-14 19:37:43 +0530

    Line

--- Turtle/main.py
+++ Turtle/main.py
@@ -3,15 +3,25 @@ from turtle import Turtle, Screen
 the_turtle = Turtle()
 
 #  SQUARE  START
-the_turtle.shape("turtle")
-the_turtle.color("red")
-
-for _ in range(4):
-    the_turtle.forward(100)
-    the_turtle.right(90)
-
+# the_turtle.shape("turtle")
+# the_turtle.color("red")
+#
+# for _ in range(4):
+#     the_turtle.forward(100)
+#     the_turtle.right(90)
 #  END
 
+# Drawing a line  START
+for _ in range(15):
+    the_turtle.forward(10)
+
+
+for _ in range(15):
+    the_turtle.forward(10)
+    the_turtle.penup()
+    the_turtle.forward(10)
+    the_turtle.pendown()
+# END
 
 screen = Screen()
 screen.exitonclick()
