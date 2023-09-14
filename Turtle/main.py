cbe48caf0bfda01ed2d440ee8cbf97014373e9df
borkarSahil <officialsahilborkar@gmail.com>
2023-09-15 00:10:05 +0530

    Shapes

--- Turtle/main.py
+++ Turtle/main.py
@@ -12,16 +12,28 @@ the_turtle = Turtle()
 #  END
 
 # Drawing a line  START
-for _ in range(15):
-    the_turtle.forward(10)
-
-
-for _ in range(15):
-    the_turtle.forward(10)
-    the_turtle.penup()
-    the_turtle.forward(10)
-    the_turtle.pendown()
+# for _ in range(15):
+#     the_turtle.forward(10)
+#
+#
+# for _ in range(15):
+#     the_turtle.forward(10)
+#     the_turtle.penup()
+#     the_turtle.forward(10)
+#     the_turtle.pendown()
 # END
+
+# Start
+
+for num_sides in range(3,10):
+    angle = 360 / num_sides
+    for j in range(num_sides):
+        the_turtle.forward(100)
+        the_turtle.right(angle)
+    num_sides += 1
+
+
+# End
 
 screen = Screen()
 screen.exitonclick()
